dcf241b984bc4ac29d238fb8857e220ce9999960
FelixMaetzler <felixmaetzer@gmx.de>
2023-12-06 07:13:56 +0100

    optimized day 06

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,26 +1,37 @@
 advent_of_code::solution!(6);
 
-pub fn part_one(input: &str) -> Option<usize> {
+pub fn part_one(input: &str) -> Option<u64> {
     let (times, distances) = parse_part_one(input);
     Some(
         times
             .into_iter()
-            .zip(distances.into_iter())
-            .map(|(t, d)| winning_count(t, d))
+            .zip(distances)
+            .map(|(t, d)| solve(t, d))
             .product(),
     )
 }
-fn winning_count(time: u64, distance: u64) -> usize {
-    (0..=time).filter(|t| winnable(*t, time, distance)).count()
+fn solve(time: u64, distance: u64) -> u64 {
+    // can be solved with PQ Formula
+    // d = speed * (time - t) > distance
+    // speed is in this case just t
+    // t * (time - t) > distance
+    // t^2 - time * t + distance < 0
+    // therfore the following formula
+    let time = time as f64;
+    let distance = distance as f64;
+    let t1 = (time / 2.0) - (((time * time) / 4.0) - distance).sqrt();
+    let t2 = (time / 2.0) + (((time * time) / 4.0) - distance).sqrt();
+    // we know that the function is smaller than zero between the two zeros
+    // so we know we have to round t1 up and round t2 down because than we are negative (< 0)
+    let t1 = t1.ceil() as u64;
+    let t2 = t2.floor() as u64;
+    // if t1=3 and t2=4 we have two possible solutions.
+    // Therefore the +1
+    t2 - t1 + 1
 }
-fn winnable(t: u64, max_time: u64, distance: u64) -> bool {
-    let speed = t;
-    let d = speed * (max_time - t);
-    d > distance
-}
-pub fn part_two(input: &str) -> Option<usize> {
+pub fn part_two(input: &str) -> Option<u64> {
     let (time, distance) = parse_part_two(input);
-    Some(winning_count(time, distance))
+    Some(solve(time, distance))
 }
 fn parse_part_one(input: &str) -> (Vec<u64>, Vec<u64>) {
     let mut it = input.trim().lines();
@@ -65,10 +76,19 @@ mod tests {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(288));
     }
-
+    #[test]
+    fn test_part_one_actual() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(608_902));
+    }
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(71503));
+        assert_eq!(result, Some(71_503));
+    }
+    #[test]
+    fn test_part_two_actual() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(46_173_809));
     }
 }
