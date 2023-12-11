e2d5aff94dd48755867c077a98cc5e12c99e2dff
FelixMaetzler <felixmaetzer@gmx.de>
2023-12-11 10:16:26 +0100

    solved day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, fmt::Debug};
 
 use advent_of_code::{Grid, OwnIndex};
 
@@ -28,7 +28,22 @@ impl Debug for Tile {
     }
 }
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut grid = parse(input);
+    solve(input, 2)
+}
+fn distance(n: ((usize, usize), (usize, usize))) -> usize {
+    let (x, y) = n;
+    x.0.abs_diff(y.0) + x.1.abs_diff(y.1)
+}
+fn build_combinations(vec: &[(usize, usize)]) -> Vec<((usize, usize), (usize, usize))> {
+    (0..vec.len() - 1)
+        .flat_map(|i| (i + 1..vec.len()).map(move |j| (vec[i], vec[j])))
+        .collect()
+}
+pub fn part_two(input: &str) -> Option<usize> {
+    solve(input, 1_000_000)
+}
+fn solve(input: &str, times: usize) -> Option<usize> {
+    let grid = parse(input);
 
     let empty_rows = (0..grid.height())
         .map(|y| (y, grid.get_row(y)))
@@ -40,37 +55,33 @@ pub fn part_one(input: &str) -> Option<usize> {
         .filter(|(_, v)| v.iter().all(|t| t == &Tile::Space))
         .map(|(x, _)| x)
         .collect::<Vec<_>>();
-
-    empty_rows
-        .iter()
-        .rev()
-        .for_each(|y| grid.insert_row(*y, &vec![Tile::Space; grid.width()]));
-
-    empty_cols
-        .iter()
-        .rev()
-        .for_each(|x| grid.insert_col(*x, &vec![Tile::Space; grid.height()]));
-
     let galaxies = grid
         .iter()
         .enumerate()
         .filter(|(_, t)| t == &&Tile::Galaxy)
         .map(|(n, _)| n.to_2d_index(&grid))
         .collect::<Vec<_>>();
-    let combinations = build_combinations(&galaxies);
+    let mut map_rows: HashMap<usize, usize> =
+        HashMap::from_iter((0..grid.height()).map(|n| (n, n)));
+    let mut map_cols: HashMap<usize, usize> = HashMap::from_iter((0..grid.width()).map(|n| (n, n)));
+    empty_rows.iter().for_each(|n| {
+        map_rows
+            .iter_mut()
+            .filter(|(k, _)| k > &n)
+            .for_each(|(_, v)| *v += times - 1)
+    });
+    empty_cols.iter().for_each(|n| {
+        map_cols
+            .iter_mut()
+            .filter(|(k, _)| k > &n)
+            .for_each(|(_, v)| *v += times - 1)
+    });
+    let new_galaxies = galaxies
+        .into_iter()
+        .map(|(y, x)| (*map_rows.get(&y).unwrap(), *map_cols.get(&x).unwrap()))
+        .collect::<Vec<_>>();
+    let combinations = build_combinations(&new_galaxies);
     Some(combinations.into_iter().map(distance).sum())
-}
-fn distance(n: ((usize, usize), (usize, usize))) -> usize {
-    let (x, y) = n;
-    x.0.abs_diff(y.0) + x.1.abs_diff(y.1)
-}
-fn build_combinations(vec: &[(usize, usize)]) -> Vec<((usize, usize), (usize, usize))> {
-    (0..vec.len() - 1)
-        .flat_map(|i| (i + 1..vec.len()).map(move |j| (vec[i], vec[j])))
-        .collect()
-}
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 fn parse(input: &str) -> Grid<Tile> {
     Grid::from_iter_iter(
@@ -97,7 +108,14 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = solve(&advent_of_code::template::read_file("examples", DAY), 10);
+        assert_eq!(result, Some(1_030));
+        let result = solve(&advent_of_code::template::read_file("examples", DAY), 100);
+        assert_eq!(result, Some(8_410));
+    }
+    #[test]
+    fn test_part_two_actual() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(603_020_563_700));
     }
 }
